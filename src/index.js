7997b00b098a624ce313c49ed5de067b72ba7107
joannaberlin <joanna.kot.ok@gmail.com>
2022-11-15 09:36:05 +0100

    Change index.js to remove error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
